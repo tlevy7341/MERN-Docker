45b3b5426d1efaf32c701a7e3563b572a5749d52
Tyrail Leverett <tyrailleverett@gmail.com>
2022-01-15 00:31:27 -0600

    Updated server to typescript

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,27 +1,29 @@
-import express, {Application, Request, Response, NextFunction } from 'express';
+import express, { Express } from 'express';
+import http from "http";
+import helmet from "helmet";
 import cors from 'cors';
+import {routes} from "./routes/route";
 const mongoose = require('mongoose');
+const router: Express = express();
 
 
-const app: Application = express();
-app.use(express.json());
-app.use(cors());
-const PORT: string = '3001';
+router.use(express.json());
+router.use(express.urlencoded({ extended: false }));
+router.use(helmet());
+router.use(cors());
+router.use(routes);
 
 
 //Connect to DB
-//const uri: string = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGODB_HOSTNAME}:27017/${process.env.MONGO_DATABASE}`
 const uri: string = 'mongodb://mongo:27017/webapp'
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
-
 
  mongoose
   .connect(uri, options)
   .then(() => console.log("Database connected..."))
 
 
-app.get('/', (req: Request, res: Response) => {
-    res.send({message: 'Hello World'});
-})
 
-app.listen(PORT, ()=> console.log(`Server running on http://localhost:${PORT}`));
+const httpServer = http.createServer(router);
+const PORT: string = '3001';
+httpServer.listen(PORT, ()=> console.log(`Server running on http://localhost:${PORT}`));
