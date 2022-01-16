cc2bb1575d7c9f74547a3061b0c0b4aec2b38c38
Tyrail Leverett <tyrailleverett@gmail.com>
2022-01-15 23:56:32 -0600

    added comments

--- server/src/controllers/controller.ts
+++ server/src/controllers/controller.ts
@@ -1,5 +1,7 @@
+/** Imports */
 import {Request, Response} from "express";
 
+/** Get all items */
 export const getAll = (req: Request, res: Response) => {
   return res.status(200).json({'message': 'Hello World'})
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,4 @@
+/** Imports */
 import express, { Express } from 'express';
 import http from "http";
 import helmet from "helmet";
@@ -6,15 +7,18 @@ import {routes} from "./routes/route";
 const mongoose = require('mongoose');
 const router: Express = express();
 
-
+/** App setup */
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 router.use(helmet());
 router.use(cors());
+
+
+/** Routes */
 router.use(routes);
 
 
-//Connect to DB
+/** Connect to DB */
 const URI = process.env.mongo_url;
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 
@@ -22,6 +26,8 @@ mongoose
   .connect(URI, options)
   .then(() => console.log("Database connected...")) 
 
+ 
+/** Server */
 const httpServer = http.createServer(router);
 const PORT: string = '3001';
 httpServer.listen(PORT, ()=> console.log(`Server running on http://localhost:${PORT}`));
